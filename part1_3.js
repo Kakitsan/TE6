5f590d88ed75ba62fd7502a8fa0e84e7166003c4
Kakitsan <zengkakit1116@gmail.com>
2021-10-10 21:27:47 -0700

    1

--- part1_3.js
+++ part1_3.js
@@ -1,9 +1,9 @@
-let z = 8;
+let number = 8;
 
 let canvas = "";
 
-for (let y = 0; y < z; y++) {
-  for (let x = 0; x < z; x++) {
+for (let y = 0; y < number; y++) {
+  for (let x = 0; x < number; x++) {
     if ((x + y) % 2 == 0) {
       canvas += " ";
     } else {
